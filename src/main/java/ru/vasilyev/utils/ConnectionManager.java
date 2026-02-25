a4cbcc0a1291daa0aff7ff21c293ff8229ed9eb3
catinasw <alexandrvasilev12rus@gmail.com>
2026-02-25 15:09:47 +0300

    implemented connection pool

--- src/main/java/ru/vasilyev/utils/ConnectionManager.java
+++ src/main/java/ru/vasilyev/utils/ConnectionManager.java
@@ -1,24 +1,72 @@
 package ru.vasilyev.utils;
 
+import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 public final class ConnectionManager {
     private static final String URL_KEY = "db.url";
     private static final String USER_KEY = "db.user";
     private static final String PASSWORD_KEY = "db.password";
+    private static final String POOL_SIZE_KEY = "db.connectionPoolSize";
+    private static final int DEFAULT_POOL_SIZE = 20;
+    private static BlockingQueue<Connection>  connectionPool;
+    private static List<Connection> sourceConnections;
+
+    static {
+        initConnectionPool();
+    }
 
     private ConnectionManager() {
     }
 
+    private static void initConnectionPool() {
+        String poolSize = PropertiesUtil.get(POOL_SIZE_KEY);
+        int size = poolSize == null ? DEFAULT_POOL_SIZE : Integer.parseInt(poolSize);
+        connectionPool = new ArrayBlockingQueue<>(size);
+        sourceConnections = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            Connection connection = open();
+            Connection connectionProxy = (Connection) Proxy.newProxyInstance(
+                    ConnectionManager.class.getClassLoader(),
+                    new Class[]{Connection.class},
+                    (proxy, method, args) -> method.getName().equals("close") ?
+                            connectionPool.add((Connection) proxy) : method.invoke(connection, args));
+            connectionPool.add(connectionProxy);
+            sourceConnections.add(connection);
+        }
+    }
+
     public static Connection get() {
+        try {
+            return connectionPool.take();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Connection open() {
         try {
             return DriverManager.getConnection(
                     PropertiesUtil.get(URL_KEY),
                     PropertiesUtil.get(USER_KEY),
                     PropertiesUtil.get(PASSWORD_KEY)
             );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void closePool() {
+        try {
+            for (Connection connection : sourceConnections) {
+                connection.close();
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
