d00900d8b3aed8bc89dfd79d33836ac0af39b5aa
catinasw <alexandrvasilev12rus@gmail.com>
2026-02-25 13:37:10 +0300

    added all args constr

--- src/main/java/ru/vasilyev/models/Currency.java
+++ src/main/java/ru/vasilyev/models/Currency.java
@@ -1,11 +1,13 @@
 package ru.vasilyev.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class Currency {
     private int id;
 
