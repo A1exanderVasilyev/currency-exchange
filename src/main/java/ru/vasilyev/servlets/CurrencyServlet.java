faa329edc69e273786d0fc46666647047aaa69a3
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-04 14:15:17 +0300

    implemented currency servlet

---
+++ src/main/java/ru/vasilyev/servlets/CurrencyServlet.java
@@ -0,0 +1,43 @@
+package ru.vasilyev.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import ru.vasilyev.models.Currency;
+import ru.vasilyev.services.CurrencyService;
+import ru.vasilyev.utils.Utils;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+@WebServlet("/currency/*")
+public class CurrencyServlet extends HttpServlet {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final CurrencyService currencyService = CurrencyService.getInstance();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        if (path == null || path.equals("/")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Currency code is missing");
+            return;
+        }
+
+        String currencyCode = Utils.getCurrencyCodeFromPath(path);
+        try {
+            Currency currency = currencyService.getCurrencyByCode(currencyCode);
+            if (currency == null) {
+                resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Currency not found");
+                return;
+            }
+            resp.setContentType("application/json");
+            resp.setCharacterEncoding("UTF-8");
+            objectMapper.writeValue(resp.getOutputStream(), currency);
+
+        } catch (RuntimeException e) {
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
