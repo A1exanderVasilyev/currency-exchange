1c148991a0c4f595e7aaf2e41ea6a829908f817c
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-04 14:08:32 +0300

    change throwing exeption type

--- src/main/java/ru/vasilyev/dao/CurrencyDAO.java
+++ src/main/java/ru/vasilyev/dao/CurrencyDAO.java
@@ -6,5 +6,5 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 public interface CurrencyDAO extends DAO<Currency> {
-    Optional<Currency> findByCode(String code) throws SQLException;
+    Optional<Currency> findByCode(String code) throws RuntimeException;
 }
