80e31ae1eb8c52f86156baaea11111b68b3dbaaa
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-05 17:35:00 +0300

    implemented doPatch method with request handler

--- src/main/java/ru/vasilyev/servlets/ExchangeRateServlet.java
+++ src/main/java/ru/vasilyev/servlets/ExchangeRateServlet.java
@@ -11,6 +11,10 @@ import ru.vasilyev.utils.Utils;
 import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.function.Function;
+
+import static ru.vasilyev.utils.Utils.parseRateToBigDecimal;
 
 @WebServlet("/exchangeRate/*")
 public class ExchangeRateServlet extends HttpServlet {
@@ -19,33 +23,77 @@ public class ExchangeRateServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handleRequest(req, resp, this::processGet);
+    }
+
+    @Override
+    protected void doPatch(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        handleRequest(req, resp, exchangeRate -> processPatch(req, exchangeRate));
+    }
+
+    private void handleRequest(HttpServletRequest req,
+                               HttpServletResponse resp,
+                               Function<ExchangeRate, ExchangeRate> process) throws IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
-        String path = req.getPathInfo();
-        if (isPathCodePairNotValid(path)) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid exchange rate code pair");
+        String[] exchangeRatePair = extractExchangeRatePair(req, resp);
+        if (exchangeRatePair == null) {
             return;
         }
-        String[] exchangeRatePair = Utils.getCurrencyPairFromPath(path);
 
         try {
             ExchangeRate exchangeRate = exchangeRateService
-                    .getExchangeRateByCodePair(exchangeRatePair[0],  exchangeRatePair[1]);
+                    .getExchangeRateByCodePair(exchangeRatePair[0], exchangeRatePair[1]);
             if (exchangeRate == null) {
                 resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Exchange rate not found");
                 return;
             }
-            objectMapper.writeValue(resp.getOutputStream(), exchangeRate);
+
+            ExchangeRate result = process.apply(exchangeRate);
+            objectMapper.writeValue(resp.getOutputStream(), result);
+        } catch (IllegalArgumentException e) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
         } catch (Exception e) {
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
                     "Internal server error");
         }
     }
 
-    private boolean isPathCodePairNotValid(String path) {
+    private ExchangeRate processGet(ExchangeRate exchangeRate) {
+        return exchangeRate;
+    }
+
+    private ExchangeRate processPatch(HttpServletRequest req,
+                                      ExchangeRate exchangeRate) {
+        String rate = req.getParameter("rate");
+        if (rate == null || rate.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid exchange rate code");
+        }
+        BigDecimal bigDecimalRate = parseRateToBigDecimal(rate.trim().replaceAll(",", "."));
+        if (bigDecimalRate == null) {
+            throw new IllegalArgumentException("Invalid exchange rate code");
+        }
+        exchangeRate.setRate(bigDecimalRate);
+        exchangeRateService.updateExchangeRate(exchangeRate);
+        return exchangeRate;
+    }
+
+    private String[] extractExchangeRatePair(HttpServletRequest req,
+                                             HttpServletResponse resp) throws IOException {
+        String path = req.getPathInfo();
+        if (!isPathCodePairValid(path)) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid exchange rate code pair");
+            return null;
+        }
+        return Utils.getCurrencyPairFromPath(path);
+    }
+
+
+    private boolean isPathCodePairValid(String path) {
         // example: /USDRUB
-        int pathWithCodePairLength = 7;
-        return path == null || path.isEmpty() || path.length() < pathWithCodePairLength;
+        int codePairLength = 7;
+        if (path == null || path.isEmpty()) return false;
+        return path.length() >= codePairLength && path.matches("/[a-zA-Z]{6}");
     }
 }
