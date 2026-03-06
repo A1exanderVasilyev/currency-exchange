6d2a54904330a0a84eea1682492d048088997bbb
catinasw <alexandrvasilev12rus@gmail.com>
2026-03-06 12:36:09 +0300

    remove throwing exception

--- src/main/java/ru/vasilyev/dao/ExchangeRateDAO.java
+++ src/main/java/ru/vasilyev/dao/ExchangeRateDAO.java
@@ -2,12 +2,11 @@ package ru.vasilyev.dao;
 
 import ru.vasilyev.models.ExchangeRate;
 
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
 public interface ExchangeRateDAO extends DAO<ExchangeRate> {
     Optional<ExchangeRate> findByCodePair(String baseCurrencyCode,
                                           String targetCurrencyCode);
-    List<ExchangeRate> findAllPairsWithBaseCurrency(String baseCurrencyCode) throws SQLException;
+    List<ExchangeRate> findAllPairsWithBaseCurrency(String baseCurrencyCode);
 }
